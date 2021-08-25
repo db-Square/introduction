23393a79041d2cfb41c4e806351c948af4d3eba3
UI Lbbw Support <uilbbwsupport@razor-risk.com>
2021-08-25 08:04:38 +0530

    update more information on profile

--- introduction/tailwind.config.js
+++ introduction/tailwind.config.js
@@ -1,11 +1,18 @@
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: colors.cyan,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
